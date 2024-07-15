5f36ac996f728769b1bc70e723a3e373ce13b0e0
ayushwadhai2003 <ayushwadhai443@gmail.com>
2024-07-15 10:33:25 +0530

    first myntra commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+
+    return (
+        <div>
+            <h1 className='text-blue-500 text-2xl'>Myntra Clone</h1>
+            <p>Creater Ayush</p>
+        </div>
+    )
+}
+
+export default App
+
+
